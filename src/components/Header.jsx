ee69bc9d0982e6969d730df33d95ae20f573fe16
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2024-09-21 21:13:37 -0700

    fix path and add git ignore

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header id="header">
+            <a href="#hero">Luiz Eduardo</a>
+            <nav>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
