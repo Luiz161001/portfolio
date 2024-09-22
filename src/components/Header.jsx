5917c0bea2bd758f52341c0c7f0511f8e66b8833
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2024-09-21 21:23:29 -0700

    implemented header.jsx with basic styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
     return (
         <header id="header">
-            <a href="#hero">Luiz Eduardo</a>
+            <div id="homeLink">
+                <a href="#hero">Luiz Eduardo</a>
+            </div>
             <nav>
                 <ul>
                     <li><a href="#about">About</a></li>
