164082babe760c4a3bd6a3bb9338e39e3529bc9e
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2024-11-14 21:34:32 -0800

    changing hero style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
                     <p className="text text0">Hi, my name is</p>
                     <p className="text text1">Luiz Eduardo</p>
-                    <p className="text text2">I'm a Full Stack Developer</p>
+                    <p className="text text2">& I'm a <span className="text-color">Full Stack Developer</span></p>
                     <p className="text text3">I love learning new stuff and building things. When I'm not coding, I will be either at the gym lifting weights or reading books!</p>
 
                     <div className="btn-container">
