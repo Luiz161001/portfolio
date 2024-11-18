63c237742390f32353e850921a3936c60ad32ddf
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2024-11-18 12:58:33 -0800

    fix some css bugs

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className=".about-container">
+            <div className="left">
+
+            </div>
+            <div className="rig">
+
+            </div>
+        </div>
+    )
+}
+
+export default About;
