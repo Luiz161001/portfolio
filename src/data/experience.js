c7353186de12f29166966c6ca91a95395acd88c3
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2025-01-20 12:19:22 -0800

    adding experience data (incomplete)

---
+++ src/data/experience.js
@@ -0,0 +1,24 @@
+const experiences = [
+    {
+        company: "SQL Research Project",
+        position: "Full stack developer",
+        skills: []
+    },
+    {
+        company: "Langara College",
+        position: "Teacher Assistant (Marker)",
+        skills: []
+    },
+    {
+        company: "Goodlife Fitness",
+        position: "Club Attendant",
+        skills: []
+    },
+    {
+        company: "Nike",
+        position: "Sales Associate",
+        skills: []
+    }
+];
+
+export default experiences; 
