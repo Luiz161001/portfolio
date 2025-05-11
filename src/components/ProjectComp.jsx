86c1fc426d57db62789daccbe7c9ef21226c1cd4
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2025-05-10 17:47:46 -0700

    add some details

--- src/components/ProjectComp.jsx
+++ src/components/ProjectComp.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const ProjectComp = ({ project }) => {
+
+    // Function to returun jsx code for collaborators
+    const renderCollaborators = () => {
+        return (
+            <ul>
+                {project.members.map((member, index) => {
+                    return (
+                        <li key={index} >
+                            <a href={member.github}>
+                                <img src="https://www.vectorlogo.zone/logos/github/github-tile.svg" alt="react-icon" />
+                                {member.name}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    };
     return (
         <div className="project-container">
             <div className="project-images">
@@ -8,20 +26,38 @@ const ProjectComp = ({ project }) => {
             </div>
             <div className="project-info">
                 <h1>{project.name}</h1>
-                <p>{project.description}</p>    
-                <div  className="project-collaborators">
+                <p>{project.description}</p>
+
+
+                <div className="project-collaborators">
                     <h3>Collaborators:</h3>
-                    {project.members.map((member, index) => {
-                        return (
-                            <a key={index} href={member.github}>{member.name}</a>
-                        );
-                    })}
+                    {project.members.length > 0 ? renderCollaborators() : <p>No collaborators</p>}
                 </div>
+
+                <div className="project-technologies">
+                    <h3>Technologies:</h3>
+                    <ul>
+                        {project.technologies.map((tech, index) => {
+                            return (
+                                <li key={index} >
+                                    {tech}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+
                 <div className="project-links">
-                    {project.url ? <a href="">Live</a> : <p>Not deployed</p>}
-                    <a href={project.repo}>Code Source</a>
+                    {project.url ? <a href={project.url} className="link-btn">
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />Live</a>
+                        :
+                        <a className="link-btn">Not deployed</a>}
+                    <a href={project.repo}>
+                        <img src="https://www.vectorlogo.zone/logos/github/github-tile.svg" alt="react-icon" />
+                        Code
+                    </a>
                 </div>
-            </div>             
+            </div>
         </div>
     )
 }
